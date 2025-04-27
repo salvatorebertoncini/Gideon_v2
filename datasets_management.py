48cf13fc95ffa47ff16b49096448142f70d4095e
salvatorebertoncini <stuffs@salvatorebertoncini.com>
2025-04-27 11:04:43 +0200

    initialization of list into matrix

--- datasets_management.py
+++ datasets_management.py
@@ -1,3 +1,6 @@
+# custom imports
+import numpy as np
+
 def open_file(path:str):
     file = open(path, 'r')
     file_to_read = file.read()
@@ -9,9 +12,12 @@ def read_dataset(dataset_name:str):
     dataset_path:str = "./datasets/"
     dataset_extension:str = ".txt"
     
-    dataset = open_file(dataset_path + dataset_name + dataset_extension) 
-    
-    print(dataset_name)
-    print(dataset)
+    dataset_raw_txt = open_file(dataset_path + dataset_name + dataset_extension) 
+    dataset = dataset_raw_txt.split()
 
+    # dataset is returned as a list in which the odd index elements are the user permissions
+    # and the even index elements are the user IDs.
+    dataset_user_permissions = dataset[1::2]
+    dataset_user_IDs = dataset[::2]
 
+    return dataset_user_IDs, dataset_user_permissions
--- main.py
+++ main.py
@@ -9,9 +9,14 @@ if __name__ == "__main__":
     # parameters definition
     dataset_name = "test_dataset"
 
+    # vars initialization
+    dataset_user_IDs = None
+    dataset_user_permissions = None
+
     time_start = time.perf_counter()
     
     # dataset import
-    datasets_management.read_dataset(dataset_name)
+    dataset_user_IDs, dataset_user_permissions  = datasets_management.read_dataset(dataset_name)
+    print(dataset_user_IDs, dataset_user_permissions)
 
     print(f'time taken: {(time.perf_counter() - time_start):.6f} seconds')
